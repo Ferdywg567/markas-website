9830dbba7f0d3fa0855bec0394c8ad2d77d38056
anxdre <anxdre@protonmail.com>
2023-11-22 00:04:58 +0700

    Add responsive navbar

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use Dentro\Yalr\Attributes\Get;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     #[Get('/','home')]
-    function index(){
-
+    function index(): View
+    {
+        return view('home');
     }
 }
