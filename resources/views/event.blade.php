06ea28fc404eb83cd166e7680375218c8f62e72d
Ferdyansyah Nur Rohim <ferdywg567@gmail.com>
2023-11-22 21:46:04 +0700

    rename : home.blade > layout.blade

--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -13,7 +13,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lexend+Deca&display=swap" rel="stylesheet">
     @stack('post-style')
-    <title>Markas</title>
+    <title>Markas - Event</title>
 </head>
 
 <body>
